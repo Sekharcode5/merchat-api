11d3f2ade96dda25d7606a8369ebb7a36fca8dfc
vadlapudiSEKHAR <127579203+Sekharcode5@users.noreply.github.com>
2024-08-05 23:51:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
   credentials: true,
-  origin: process.env.CLIENT_URL,
+  origin: "https://merchat-api-j2lm.vercel.app/",
 }));
 
 async function getUserDataFromRequest(req) {
